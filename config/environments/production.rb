ce01842cd23e0d83a7d9239c3ae8233af2b00e16
LucaBla <me@lucablazevic.dev>
2023-04-08 03:55:44 +0200

    add action_mailer config to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "todo_app_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'dailydrillmailer@gmail.com',
+    password: ENV["APP_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
